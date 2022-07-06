d8aba992f34e719b836957e31cdb9914b248c70a
Pushkar Singh <stoazest@gmail.com>
2022-07-06 13:33:00 +0530

    first deploy

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc0b4bce268a67ed"; }
+/******/ 		__webpack_require__.h = function() { return "c90b1203f4753239"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
   return(
-    <h1>Hello World</h1>
+    <h1>This is a very simple notes app with Nextjs</h1>
   )
 };
 
